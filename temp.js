ebdd7ed5b364c16ab52367a65d22b6ff5ba4a011
AbbErjKad <Erjon.Kadriu@abbindustrigymnasium.se>
2022-09-08 16:01:17 +0200

    changes made

--- temp.js
+++ temp.js
@@ -5,17 +5,17 @@ let temp1 = database.ref('temps/temp1')
 let humid1 = database.ref('humidity/humidity1')
 
 //funktion med properties och methods för att hämta värden från firebase
-temp1.on('value', function (snapshot) {
+temp1.on('value', function(snapshot) {
     updateTemp1(snapshot.val())
 })
 
 
-humid1.on('value', function (snapshot) {
+humid1.on('value', function(snapshot) {
     updateHumid1(snapshot.val())
 })
 
 //funktion för att uppdatera temperatur och avrunda till 1 decimal
-function updateTemp1 (value) {
+function updateTemp1(value) {
     let t = document.getElementById('temp1')
     if (t != null) {
         t.innerHTML = Math.round(value * 10) / 10 + ('°C')
@@ -23,7 +23,7 @@ function updateTemp1 (value) {
 }
 
 //funktion för att uppdatera fuktighet och avrunda till 1 decimal
-function updateHumid1 (value) {
+function updateHumid1(value) {
     let t = document.getElementById('humidity1')
     if (t != null) {
         t.innerHTML = Math.round(value * 10) / 10 + ('%')
@@ -31,62 +31,19 @@ function updateHumid1 (value) {
 }
 
 
-// GRAF
-let tempData = [
-    ['Year', 'Temperatur']
-]
 
-let todaysTemps = database.ref('temp-time/temp-time1/' + getCurrentDate()).limitToLast(24)
-
-todaysTemps.on('value', function (snapshot) {
-    console.log(snapshot.val())
-    updateGraf(snapshot.val())
-})
-
-google.charts.load('current', { packages: ['corechart', 'line'] })
-google.charts.setOnLoadCallback(drawChart)
-
-function getCurrentDate () {
-    var today = new Date()
-    var dd = String(today.getDate()).padStart(2, '0')
-    var mm = String(today.getMonth() + 1).padStart(2, '0')
-    var yyyy = today.getFullYear()
-
-    today = yyyy + '-' + mm + '-' + dd
-    return today
-}
-
-function updateGraf (value) {
-    let grafKlass = Object.entries(value)
-    tempData.push(...grafKlass)
-    drawChart(tempData)
-}
-
-function drawChart (list) {
-
-    var dataKlass = google.visualization.arrayToDataTable(list)
-
-    var options = {
-        title: 'Senaste 24 timmar',
-        curveType: 'function',
-        legend: { position: 'bottom' },
-    }
-
-    var chart = new google.visualization.LineChart(document.getElementById('container1'))
-
-    chart.draw(dataKlass, options)
-}
-
-function openNav () {
+function openNav() {
     document.getElementById("mySidenav").style.width = "250px"
 }
 
-function closeNav () {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0"
 }
 
 let power = false
-function writeUserData () {
+let water = false
+
+function writeUserData() {
     console.log(power)
     firebase.database().ref('power').set({
         power: power
@@ -103,5 +60,69 @@ function writeUserData () {
     } else if (power == false) {
         power = true
     }
+}
+
+function writeUserData1() {
+    console.log(water)
+    firebase.database().ref('water').set({
+        water: water
+    })
+    if (water == true) {
+        document.getElementById('water').innerHTML = "Water is ON"
+
+    } else if (water == false) {
+        document.getElementById('water').innerHTML = "Water is OFF"
+
+    }
+    if (water == true) {
+        water = false
+    } else if (water == false) {
+        water = true
+    }
 
 }
+// // GRAF
+// let tempData = [
+//     ['Year', 'Temperatur']
+// ]
+
+// let todaysTemps = database.ref('temp-time/temp-time1/' + getCurrentDate()).limitToLast(24)
+
+// todaysTemps.on('value', function (snapshot) {
+//     console.log(snapshot.val())
+//     updateGraf(snapshot.val())
+// })
+
+// google.charts.load('current', { packages: ['corechart', 'line'] })
+// google.charts.setOnLoadCallback(drawChart)
+
+// function getCurrentDate () {
+//     var today = new Date()
+//     var dd = String(today.getDate()).padStart(2, '0')
+//     var mm = String(today.getMonth() + 1).padStart(2, '0')
+//     var yyyy = today.getFullYear()
+
+//     today = yyyy + '-' + mm + '-' + dd
+//     return today
+// }
+
+// function updateGraf (value) {
+//     let grafKlass = Object.entries(value)
+//     tempData.push(...grafKlass)
+//     drawChart(tempData)
+// }
+
+// function drawChart (list) {
+
+//     var dataKlass = google.visualization.arrayToDataTable(list)
+
+//     var options = {
+//         title: 'Senaste 24 timmar',
+//         curveType: 'function',
+//         legend: { position: 'bottom' },
+//     }
+
+//     var chart = new google.visualization.LineChart(document.getElementById('container1'))
+
+//     chart.draw(dataKlass, options)
+// }
