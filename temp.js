9fbd6915e904fc935637842a957f788eb4f42552
abbmalbel <69853581+abbmalbel@users.noreply.github.com>
2022-09-08 15:40:48 +0200

    ny
    Co-authored-by: abbsamoze <abbsamoze@users.noreply.github.com>

--- temp.js
+++ temp.js
@@ -1,21 +1,21 @@
 //KLASSRUMMET-VÄRDEN
 //variablar som refererar till firebase
 let database = firebase.database()
-let temp1 = database.ref('temps/temp1');
+let temp1 = database.ref('temps/temp1')
 let humid1 = database.ref('humidity/humidity1')
 
 //funktion med properties och methods för att hämta värden från firebase
-temp1.on('value', function(snapshot) {
-    updateTemp1(snapshot.val());
-});
+temp1.on('value', function (snapshot) {
+    updateTemp1(snapshot.val())
+})
 
 
-humid1.on('value', function(snapshot) {
-    updateHumid1(snapshot.val());
-});
+humid1.on('value', function (snapshot) {
+    updateHumid1(snapshot.val())
+})
 
 //funktion för att uppdatera temperatur och avrunda till 1 decimal
-function updateTemp1(value) {
+function updateTemp1 (value) {
     let t = document.getElementById('temp1')
     if (t != null) {
         t.innerHTML = Math.round(value * 10) / 10 + ('°C')
@@ -23,7 +23,7 @@ function updateTemp1(value) {
 }
 
 //funktion för att uppdatera fuktighet och avrunda till 1 decimal
-function updateHumid1(value) {
+function updateHumid1 (value) {
     let t = document.getElementById('humidity1')
     if (t != null) {
         t.innerHTML = Math.round(value * 10) / 10 + ('%')
@@ -36,43 +36,72 @@ let tempData = [
     ['Year', 'Temperatur']
 ]
 
-let todaysTemps = database.ref('temp-time/temp-time1/' + getCurrentDate()).limitToLast(24);
+let todaysTemps = database.ref('temp-time/temp-time1/' + getCurrentDate()).limitToLast(24)
 
-todaysTemps.on('value', function(snapshot) {
+todaysTemps.on('value', function (snapshot) {
     console.log(snapshot.val())
-    updateGraf(snapshot.val());
-});
+    updateGraf(snapshot.val())
+})
 
-google.charts.load('current', { packages: ['corechart', 'line'] });
-google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', { packages: ['corechart', 'line'] })
+google.charts.setOnLoadCallback(drawChart)
 
-function getCurrentDate() {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var yyyy = today.getFullYear();
+function getCurrentDate () {
+    var today = new Date()
+    var dd = String(today.getDate()).padStart(2, '0')
+    var mm = String(today.getMonth() + 1).padStart(2, '0')
+    var yyyy = today.getFullYear()
 
-    today = yyyy + '-' + mm + '-' + dd;
+    today = yyyy + '-' + mm + '-' + dd
     return today
 }
 
-function updateGraf(value) {
+function updateGraf (value) {
     let grafKlass = Object.entries(value)
     tempData.push(...grafKlass)
     drawChart(tempData)
 }
 
-function drawChart(list) {
+function drawChart (list) {
 
-    var dataKlass = google.visualization.arrayToDataTable(list);
+    var dataKlass = google.visualization.arrayToDataTable(list)
 
     var options = {
         title: 'Senaste 24 timmar',
         curveType: 'function',
         legend: { position: 'bottom' },
-    };
+    }
 
-    var chart = new google.visualization.LineChart(document.getElementById('container1'));
+    var chart = new google.visualization.LineChart(document.getElementById('container1'))
 
-    chart.draw(dataKlass, options);
+    chart.draw(dataKlass, options)
+}
+
+function openNav () {
+    document.getElementById("mySidenav").style.width = "250px"
+}
+
+function closeNav () {
+    document.getElementById("mySidenav").style.width = "0"
+}
+
+let power = false
+function writeUserData () {
+    console.log(power)
+    firebase.database().ref('power').set({
+        power: power
+    })
+    if (power == true) {
+        document.getElementById('propell').innerHTML = "Fan is ON"
+
+    } else if (power == false) {
+        document.getElementById('propell').innerHTML = "Fan is OFF"
+
+    }
+    if (power == true) {
+        power = false
+    } else if (power == false) {
+        power = true
+    }
+
 }
